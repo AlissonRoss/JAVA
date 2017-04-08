20f4aae6712c250f73203547bf0eaa0d626d6e4e
Tenma <tobiotenma@hotmail.com>
2017-04-07 19:27:36 -0700

    ITERATION THROUGH ARRAY AND PRINTS POSITIVE VALUES

---
+++ array.java
@@ -0,0 +1,42 @@
+package lab;
+import javax.swing.*;
+
+public class array {
+	public static void main(String [] args){
+		int [] array= new int [10];
+		int sum=0;
+		//For loop that initiates the counter to 0
+		//Checks if the condition of count < array's length is met
+		//and increments the counter
+		for(int count=0;count < array.length;count++){
+			
+			String var=JOptionPane.showInputDialog("Enter 10 values.\n"
+					+ "Enter value #"
+					//Array Position 0, but Input Value will be
+					//1 in the dialog
+					+(count+1)
+					+":");
+			//Converts String input into an Integer value
+			int val=Integer.parseInt(var);
+			//Assigns value into the position inside the array
+			array[count]=val;
+			
+			}
+		//For loop to iterate through the array with the values
+		for(int count=0;count < array.length;count++){
+		
+			int arrayVal = array[count];
+			//If statement to check if the int values inside of the array are
+			//positive values and if so, it'll print that value
+			if (arrayVal >= 0){
+				JOptionPane.showMessageDialog(null, "Positive: "
+						+arrayVal);
+				//Sums to the total
+				sum+=arrayVal;
+			}
+		}
+		//Prints Total of Positive Values
+		JOptionPane.showMessageDialog(null, "Total:"+sum);
+	}
+
+}
