2aec8b648514247a0f52a038e03fcfd650b38ec0
Alisson Imelda Leiva Salazar <tobiotenma@hotmail.com>
2017-04-21 22:02:52 -0700

    Initial Skeleton

---
+++ ch7Assign/src/ch7Assign/DriverExam.java
@@ -0,0 +1,46 @@
+package ch7Assign;
+
+public class DriverExam {
+	
+	private String[] correctAns= {
+			"B","D,","A","A",
+			"C","A","B","A",
+			"C","D","B","C",
+			"D","A","D","C",
+			"C","B","D","A"
+			
+	};
+	
+	//GETTERS/ACCESSORS
+	
+	//SETTERS/MUTATORS
+	
+	
+	
+	//CONSTRUCTORS
+	public boolean passed(){
+		
+		if (){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
+	
+	public int totalCorrect(){
+		return 0;
+		
+	}
+	
+	public int totalIncorrect(){
+		return 0;
+		
+	}
+	public int[] questionsMissed(){
+		return null;
+		
+	}
+
+}
